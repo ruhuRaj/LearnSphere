02bfedfd5b185ac9c58fdd9d62b929ff5a95061b
Randhir Kumar Raj <randhirrajak9431@gmail.com>
2026-07-19 00:59:23 +0530

    fixed refresh button issues after login

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -12,7 +12,7 @@ const api = axios.create({
 // Request interceptor
 api.interceptors.request.use(
   (config) => {
-    const token = localStorage.getItem('token');
+    const token = sessionStorage.getItem('token');
 
     if (token) {
       config.headers.Authorization = `Bearer ${token}`;
@@ -32,7 +32,7 @@ api.interceptors.response.use(
   (response) => response,
   (error) => {
     if (error.response?.status === 401) {
-      localStorage.removeItem('token');
+      sessionStorage.removeItem('token');
 
       if (window.location.pathname !== '/login') {
         window.location.href = '/login';
